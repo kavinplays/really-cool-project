5f75fdda3681ee8bb3f6cbf53bd3e26c8bf13366
Kavin Shanbhag <kavinplays@icloud.com>
2021-09-05 20:15:12 +0530

    Organized actions and actually used ci/cd features

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('flol'))
+app.get('/', (req, res) => res.send('Hello World!'))
 
 // export 'app'
 module.exports = app
